46d9c539e4f78238bfac1a5c01b30dceb699d519
DimiWin <kimeldim@gmail.com>
2025-12-11 21:27:55 +0100

    init

---
+++ mplib/mp.lua
@@ -0,0 +1,13 @@
+-- Multiplayer LUA library
+-- Convenient way of including all scripts.
+
+#include "countdown.lua"
+#include "hud.lua"
+#include "eventlog.lua"
+#include "spawn.lua"
+#include "stats.lua"
+#include "teams.lua"
+#include "tools.lua"
+#include "util.lua"
+#include "spectate.lua"
+#include "ui.lua"
---
+++ prophunt2.lua
@@ -0,0 +1,73 @@
+#version 2
+#include "script/include/player.lua"
+#include "Automatic.lua"
+#include "/mplib/mp.lua"
+
+shared.hiders = {}
+
+function server.init()
+	teamsInit(2)
+	teamsSetNames({"Hiders", "Hunters"})
+	teamsSetColors{{0,1,1},{1,0,0}}
+	hudInit(true)
+end
+
+
+function server.tick()
+
+end
+
+
+function client.tick()
+
+end
+
+function client.update()
+    if IsPlayerHost(GetLocalPlayer()) then 
+        client.hostTools()
+    end
+
+end
+
+function client.hostTools()
+
+end
+
+function client.draw(dt)
+	-- during countdown, display the title of the game mode.
+	hudDrawTitle(dt, "Prophunt!")
+	hudDrawBanner(dt)
+
+	
+	local maxHunters = {}
+
+	local players = GetPlayerCount()
+
+	for i=1, math.max(players - 1, 1) do
+		maxHunters[#maxHunters+1] = {label = tostring(i).. " Hunter", value = i}
+	end
+
+	local settings = {
+	{
+		title = "",
+			items = {
+				{key = "savegame.mod.settings.time", 
+				label = "Round Lenth", info="How long one round lasts", options = {{label = "05:00", value = 5*60},{label = "07:30", value = 7.5*60}, {label = "10:00", value=10*60}, {label = "03:00", value=3*60}}},
+				{key = "savegame.mod.settings.joinHunters", 
+				label = "Join Hunters", info="Makes the hiders join the hunters once found.", options = {{label = "Enable", value = true},{label = "Disable", value = false}}},
+				{key = "savegame.mod.settings.randomTeams", 
+				label = "Random Teams", info="Once the game is started the server will assign teams by random.", options = {{label = "Enable", value = true},{label = "Disable", value = false}}},
+				{key = "savegame.mod.settings.hunters", 
+				label = "Hunters Amount", info="The amount of hunters at the beginning of a game.", options = maxHunters}
+			}
+		}
+	}
+	hudDrawGameSetup(settings)
+
+	if not teamsIsSetup() then
+		teamsDraw(dt)
+
+	
+
+	end
+end
