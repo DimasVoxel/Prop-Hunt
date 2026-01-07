4f191d6b39289aea5be928aa39c60f78cb2f5530
onethousandvolts <onethousandvolts@outlook.com>
2026-01-08 00:26:57 +0100

    Add Info HUD
    -Added info HUD for hiders
    >Uses temp values for now, marked with --DEBUG and --TODO
    
    -moved force taunt UI up a bit

--- gamelogic/client/clientHUD.lua
+++ gamelogic/client/clientHUD.lua
@@ -1,5 +1,13 @@
 #include "uiHelper.lua"
 
+<<<<<<< HEAD
+=======
+--DEBUG \/ TODO remove this
+client.barfill = 10
+client.barmax = 10
+--DEBUG /\
+
+>>>>>>> 5778252 (Add Info HUD)
 function client.draw(dt)
 	hudTick(dt)
 	eventlogDraw(dt, teamsGetPlayerColorsList())
